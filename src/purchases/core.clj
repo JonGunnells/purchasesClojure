1c6174f0da7be549f32a50c115ae69f49f35e0c1
jonathangunnells <jonathan.gunnells@yahoo.com>
2016-07-08 01:19:32 -0400

    initial commit

--- src/purchases/core.clj
+++ src/purchases/core.clj
@@ -1,8 +1,8 @@
 (ns purchases-clojure.core
   (:require [clojure.string :as str]
-            [compojure.core :as c]
             [ring.adapter.jetty :as j]
-            [hiccup.core :as h])
+            [hiccup.core :as h] 
+            [compojure.core :as c])
   (:gen-class))
 
 
@@ -20,35 +20,30 @@
 (defn purchases-html [purchases]
   [:html
    [:body 
-    [:a {:href "/"} "All"]
+    [:a {:href "/"} "All"] 
+    [:a {:href "/Jewelry"} "Jewelry"]
     [:a {:href "/Alcohol"} "Alcohol "]
     [:a {:href "/Furniture"} "Furniture "]
     [:a {:href "/Toiletries"} "Toiletries "]
     [:a {:href "/Shoes"} "Shoes "]
-    [:ol
-     (map (fn [purchase]
-             [:li (str  
-                    (get purchase "customer_id")" "  
-                    (get purchase "date")" "  
-                    (get purchase "credit_card")" "  
-                    (get purchase "cvv")" "  
-                    (get purchase "category"))])
-        purchases)]]])
+    [:ol]]])
+
+(map (fn [purchase])
+    [:li (str)] (get purchase "customer_id") " " (get purchase "date")" " (get purchase "credit_card")" " (get purchase "cvv")" " (get purchase "category")
+              purchase)
 
 (defn filter-by-category [purchases category]
-  (filter (fn [purchase] 
+  (filter (fn [purchases] 
            (= category (get purchase "category")))
     purchases))
 
 (c/defroutes app
   (c/GET "/:category{.*}" [category]
     (let [purchases (file-read)
-          purchases (if (= 0 (count category))
+          purchases (if (= "" (count category))
                       purchases
                       (filter-by-category purchases category))]
       (h/html (purchases-html purchases)))))
-
-
 
 (defonce server (atom nil))
 
